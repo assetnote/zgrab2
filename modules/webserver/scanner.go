6dad928dc0e25af4c37071d1d14fe38fca4706ba
Junn <jmacey@assetnote.io>
2024-11-12 12:31:20 +1000

    fix(modules): improve webserver module to check vulnerable versions

--- modules/webserver/scanner.go
+++ modules/webserver/scanner.go
@@ -1,9 +1,10 @@
 package webserver
 
 import (
-	"log"
+	"fmt"
 	"strings"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/zmap/zgrab2"
 	"github.com/zmap/zgrab2/lib/http"
 	http_scanner "github.com/zmap/zgrab2/modules/http"
@@ -35,14 +36,65 @@ const (
 	ServerHAProxy    ServerType = "haproxy"
 	ServerSquid      ServerType = "squid"
 	ServerCloudflare ServerType = "cloudflare"
+	ServerPrivoxy    ServerType = "privoxy"
 	ServerUnknown    ServerType = "unknown"
 )
 
+type ServerVersion struct {
+	Major int `json:"major"`
+	Minor int `json:"minor"`
+	Patch int `json:"patch"`
+}
+
+func (v *ServerVersion) isLessThanOrEqual(other *ServerVersion) bool {
+	if v.Major != other.Major {
+		return v.Major <= other.Major
+	}
+	if v.Minor != other.Minor {
+		return v.Minor <= other.Minor
+	}
+	return v.Patch <= other.Patch
+}
+
+// ProxySignature describes server versions where proxy functionality is enabled by default
+type ProxySignature struct {
+	VulnerableVersion *ServerVersion `json:"vulnerable_version"`
+	CVE               string         `json:"cve"`
+	Details           string         `json:"details"`
+	IsMatch           bool           `json:"is_match"`
+}
+
+var ServerProxySignatures = map[ServerType][]ProxySignature{
+	ServerApache: {
+		{
+			VulnerableVersion: &ServerVersion{2, 4, 48},
+			CVE:               "CVE-2021-40438",
+			Details:           "mod_proxy enabled by default, vulnerable to SSRF",
+		},
+	},
+	ServerNginx: {
+		{
+			VulnerableVersion: &ServerVersion{1, 20, 1},
+			CVE:               "CVE-2021-23017",
+			Details:           "proxy_pass enabled by default",
+		},
+	},
+	ServerPrivoxy: {
+		{
+			VulnerableVersion: &ServerVersion{3, 0, 19},
+			CVE:               "",
+			Details:           "",
+		},
+	},
+}
+
 // Results contains the findings from the scan
 type Results struct {
-	Response    *http.Response `json:"response,omitempty"`
-	ServerType  ServerType     `json:"server_type"`
-	ServerValue string         `json:"server_value,omitempty"`
+	Response       *http.Response `json:"response,omitempty"`
+	ServerType     ServerType     `json:"server_type"`
+	ServerValue    string         `json:"server_value,omitempty"`
+	Proxy          bool           `json:"proxy"`
+	ProxySignature ProxySignature `json:"proxy_signature,omitempty"`
 }
 
 // RegisterModule registers the zgrab2 module
@@ -106,28 +158,85 @@ func (s *Scanner) Protocol() string {
 	return "webserver"
 }
 
-// identifyServer examines the Server header to determine the server type
-func identifyServer(serverHeader string) (ServerType, string) {
+// parseVersion parses a version string into a VersionInfo
+func parseVersion(versionStr string) *ServerVersion {
+	// extract version number before any parentheses or spaces
+	parts := strings.Split(versionStr, " ")
+	if len(parts) == 0 {
+		return nil
+	}
+
+	// split version component (e.g., "Apache/2.4.18")
+	versionParts := strings.Split(parts[0], "/")
+	if len(versionParts) != 2 {
+		return nil
+	}
+
+	var major, minor, patch int
+	_, err := fmt.Sscanf(versionParts[1], "%d.%d.%d", &major, &minor, &patch)
+	if err != nil {
+		// try parsing just major.minor
+		if _, err := fmt.Sscanf(versionParts[1], "%d.%d", &major, &minor); err != nil {
+			return nil
+		}
+		patch = 0
+	}
+
+	return &ServerVersion{Major: major, Minor: minor, Patch: patch}
+}
+
+func identifyServer(serverHeader string) (ServerType, string, ProxySignature) {
 	if serverHeader == "" {
-		return ServerUnknown, ""
+		return ServerUnknown, "", ProxySignature{}
 	}
 
 	headerLower := strings.ToLower(serverHeader)
+	version := parseVersion(serverHeader)
 
-	switch {
-	case strings.Contains(headerLower, "apache/"):
-		return ServerApache, serverHeader
-	case strings.Contains(headerLower, "nginx/"):
-		return ServerNginx, serverHeader
-	case strings.Contains(headerLower, "haproxy/"):
-		return ServerHAProxy, serverHeader
-	case strings.Contains(headerLower, "squid/"):
-		return ServerSquid, serverHeader
-	case strings.Contains(headerLower, "cloudflare"):
-		return ServerCloudflare, serverHeader
-	default:
-		return ServerUnknown, serverHeader
+	serverTypes := map[string]ServerType{
+		"apache/":    ServerApache,
+		"nginx":      ServerNginx,
+		"haproxy/":   ServerHAProxy,
+		"squid/":     ServerSquid,
+		"cloudflare": ServerCloudflare,
 	}
+
+	for substr, serverType := range serverTypes {
+		if strings.Contains(headerLower, substr) {
+			if version != nil {
+				if configs, exists := ServerProxySignatures[serverType]; exists {
+					for _, config := range configs {
+						if version.isLessThanOrEqual(config.VulnerableVersion) {
+							config.IsMatch = true
+							return serverType, serverHeader, config
+						}
+					}
+				}
+			} else {
+				log.Warnf("failed to parse version from %s", serverHeader)
+			}
+			return serverType, serverHeader, ProxySignature{}
+		}
+	}
+
+	return ServerUnknown, serverHeader, ProxySignature{}
+}
+
+func identifyProxyAgent(proxyAgent string) (ServerType, string, ProxySignature) {
+	if strings.Contains(strings.ToLower(proxyAgent), "privoxy") {
+		if version := parseVersion(proxyAgent); version != nil {
+			if configs, exists := ServerProxySignatures[ServerPrivoxy]; exists {
+				for _, config := range configs {
+					if version.isLessThanOrEqual(config.VulnerableVersion) {
+						config.IsMatch = true
+						return ServerPrivoxy, proxyAgent, config
+					}
+				}
+			}
+		}
+		return ServerPrivoxy, proxyAgent, ProxySignature{}
+	}
+	return ServerUnknown, "", ProxySignature{}
 }
 
 // Scan performs the web server scan
@@ -163,7 +272,15 @@ func (s *Scanner) Scan(t zgrab2.ScanTarget) (zgrab2.ScanStatus, interface{}, err
 			results.Response = resp.Response
 
 			serverHeader := resp.Response.Header.Get("Server")
-			results.ServerType, results.ServerValue = identifyServer(serverHeader)
+			results.ServerType, results.ServerValue, results.ProxySignature = identifyServer(serverHeader)
+
+			// no server header so check proxy agent
+			if serverHeader == "" {
+				proxyAgent := resp.Response.Header.Get("Proxy-Agent")
+				results.ServerType, results.ServerValue, results.ProxySignature = identifyProxyAgent(proxyAgent)
+			}
+
+			results.Proxy = results.ProxySignature.IsMatch
 
 			// Only return protocol error for unknown server type if we got a valid response
 			// and it wasn't a 400 Bad Request (which might indicate HTTPS required)
